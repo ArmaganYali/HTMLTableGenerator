83ed73d8d85dfff3a90bf770e562c864d461f1bd
Armağan Dilara <yali.armagan@gmail.com>
2022-11-28 13:44:44 +0300

    output.html tablo olusturuldu.

--- HTMLTableGenerator/Program.cs
+++ HTMLTableGenerator/Program.cs
@@ -1,4 +1,27 @@
 ﻿
 
+using HTMLTableGenerator;
+using System.Text.Json;
+
 string moviesText = File.ReadAllText("movies.json");
-Console.WriteLine(moviesText);
+//Console.WriteLine(moviesText);
+
+var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+List<Movie> movies = JsonSerializer.Deserialize<List<Movie>>(moviesText, options);
+
+//foreach (var m in movies)
+//{
+//    Console.WriteLine(m.Title);
+//}
+
+// Tablo başla
+string tabloBasla = "";
+// Tablo başlıklar
+string tabloBasliklar = "";
+// Tablo gövde
+string tabloGovde = "";
+// Tablo kapat
+string tabloKapat = "";
+
+string tablo = tabloBasla + tabloBasliklar + tabloGovde + tabloKapat;
+File.WriteAllText("output.html", tablo);
